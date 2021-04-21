cbd6acf6ce55a66c04367030a576bf4ab16267c3
NIASKA <mkman751@yahoo.com>
2021-04-20 21:33:32 -0500

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let mybutton = document.getElementById("topBtn");
+var mybutton = document.getElementById("topBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};

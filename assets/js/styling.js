91c3eb257bac807d562a65f0fdd3228edd9fd502
NIASKA <mkman751@yahoo.com>
2021-04-29 15:57:50 -0500

    needs to fix button

--- assets/js/styling.js
+++ assets/js/styling.js
@@ -3,5 +3,6 @@ const tl = gsap.timeline({default: {ease: "power1.out"} });
 tl.to(".cText", {y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", {y: "0%", duration: 1.5, delay: 0.5 });
 tl.to(".coverPage", {y: "-100%", duration: 1 }, "-=1");
-tl.fromTo(".titleText", {y: "0%", duration: 1.5, delay: 2.0});
+tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1});
+tl.fromTo(".titleText", {opacity: 0}, {opacity: 1, duration: 0.5});
 

5e04475132aec1dd135ecd6049bf63fe8171b9aa
NIASKA-Desktop <mkman751@yahoo.com>
2021-05-03 16:50:26 -0300

    working slider, need css adjustments

--- assets/js/styling.js
+++ assets/js/styling.js
@@ -28,19 +28,19 @@ const navButton = () => {
 
 navButton();
 
-var swiper = new swiper(".mySwiper", {
+var swiper = new Swiper(".swiper-container", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: "auto",
     coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifer: 1,
-        slideShadows: true,
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
     },
     pagination: {
-        el: ".swiper-pagination",
-    }
-});
+      el: ".swiper-pagination",
+    },
+  });
